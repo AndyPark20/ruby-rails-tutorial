8cb6d3cf5add4e8f69cbd099ded90a65691adee3
Andy Park <apark1214@gmail.com>
2023-01-17 09:18:08 -0800

    scaffold generating

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   get "welcome" => "pages#index"
   get "about" => "pages#second_page" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
